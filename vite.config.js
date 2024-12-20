6cedbf7cbdc49d3078eaff12c837f86f0a38e02d
Sebastian Joseph <sebyjoseph2015@gmail.com>
2024-12-20 12:54:30 -0600

    change base path

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/notebook-viewer/',
   plugins: [
     vue(),
     vueDevTools(),
